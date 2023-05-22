68b63d3de7e2b645d1f054d2a7437a39947ad6ec
Chuqdi <103970327+Chuqdi@users.noreply.github.com>
2023-05-21 19:54:21 -0700

    djdjsdjsdjksdjksdj

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,11 @@
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
 
 
 
@@ -33,3 +35,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("users.urls"))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
